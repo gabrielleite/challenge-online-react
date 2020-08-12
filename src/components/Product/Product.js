c020b762ab77547302300fe306e49470fde7c4cc
Nicolás Croce <nicolas.croce@tiendanube.com>
2020-08-12 17:06:40 -0300

    feat(js): Fixed product component.

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import "./Product.scss";
 
-const Product = ({ name, count, price }) => {
+const Product = ({ name, count, price, promotionalPrice }) => {
   return (
     <div className="product">
       <span>{name}</span>
       <span>{count}</span>
       <span>{price}</span>
+      <span>{promotionalPrice}</span>
     </div>
   );
 };
