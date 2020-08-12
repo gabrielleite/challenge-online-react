937851afde952a6fac1de48a2fd2d22a3cd6e806
Nicolás Croce <nicolas.croce@tiendanube.com>
2020-08-12 17:24:23 -0300

    feat(js): Convert class to function. [ProductForm]

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,43 +1,62 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import "./components.scss";
 import Input from "./Input/Input";
 
-class ProductForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      product: "",
-      count: "",
-      price: "",
-      promotionalPrice: "",
-    };
-  }
+const ProductForm = () => {
+  const [state, setState] = useState({
+    product: "",
+    count: "",
+    price: "",
+    promotionalPrice: "",
+  });
 
-  handleInputChange = (event) => {
+  const handleInputChange = (event) => {
     const { name, value } = event.target;
-    return this.setState(prevState => ({ ...prevState, [name]: value }))
-  }
+    return setState((prevState) => ({ ...prevState, [name]: value }));
+  };
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-  }
+  };
 
-  render() {
-    return (
-      <div className="centered">
-        <h2>Add new product</h2>
-        <div>
-          <Input label="Product name" value={ this.state.product } name="product" type="text" onChange={ this.handleInputChange } />
-          <Input label="Items count" value={ this.state.count } name="count" type="text" onChange={ this.handleInputChange } />
-          <Input label="Price" value={ this.state.price } name="price" type="tel" onChange={ this.handleInputChange } />
-          <Input label="Promotional Price" value={ this.state.promotionalPrice } name="promotionalPrice" type="tel" onChange={ this.handleInputChange } />
-          
-          <input className="button" type="submit" value="Aceptar" />
-        </div>
+  return (
+    <div className="centered">
+      <h2>Add new product</h2>
+      <div>
+        <Input
+          label="Product name"
+          value={state.product}
+          name="product"
+          type="text"
+          onChange={handleInputChange}
+        />
+        <Input
+          label="Items count"
+          value={state.count}
+          name="count"
+          type="text"
+          onChange={handleInputChange}
+        />
+        <Input
+          label="Price"
+          value={state.price}
+          name="price"
+          type="tel"
+          onChange={handleInputChange}
+        />
+        <Input
+          label="Promotional Price"
+          value={state.promotionalPrice}
+          name="promotionalPrice"
+          type="tel"
+          onChange={handleInputChange}
+        />
+
+        <input className="button" type="submit" value="Aceptar" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default ProductForm;
