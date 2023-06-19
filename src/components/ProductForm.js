df16ae8d4a918076e9ba825093fc836b33c0b3b9
gabrielleite <gabrielcfcleite@gmail.com>
2023-06-19 12:54:51 -0300

    feat: improve ProductForm accessibility

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -22,14 +22,14 @@ const ProductForm = () => {
     <form className="centered" onSubmit={handleSubmit}>
       <h2>Add new product</h2>
       <div>
-        <label className="label" htmlFor="">Product name</label>
-        <input name="name" type="text" value={ product.name } onChange={handleInputChange}/>
-        <label className="label" htmlFor="">Items count</label>
-        <input name="count" type="text" value={ product.count } onChange={handleInputChange}/>
-        <label className="label" htmlFor="">Price</label>
-        <input name="price" type="text" value={ product.price } onChange={handleInputChange}/>
-        <label className="label" htmlFor="">Promotional Price</label>
-        <input name="promotionalPrice" type="text" value={ product.promotionalPrice } onChange={handleInputChange}/>
+        <label className="label" htmlFor="name">Product name</label>
+        <input id="name" name="name" type="text" value={ product.name } onChange={handleInputChange}/>
+        <label className="label" htmlFor="count">Items count</label>
+        <input id="count" name="count" type="text" value={ product.count } onChange={handleInputChange}/>
+        <label className="label" htmlFor="price">Price</label>
+        <input id="price" name="price" type="text" value={ product.price } onChange={handleInputChange}/>
+        <label className="label" htmlFor="promotionalPrice">Promotional Price</label>
+        <input id="promotionalPrice" name="promotionalPrice" type="text" value={ product.promotionalPrice } onChange={handleInputChange}/>
         <input className="button" type="submit" value="Aceptar" />
       </div>
     </form>
--- src/components/ProductForm.spec.js
+++ src/components/ProductForm.spec.js
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 
 import React from "react";
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import ProductForm from "./ProductForm";
 
 describe('ProductForm', () => {
@@ -13,5 +13,27 @@ describe('ProductForm', () => {
     it('renders correctly', () => {
         const { container } = wrapper;
         expect(container).toMatchSnapshot();
+    });
+
+    describe('accessibility interactions', () => {
+        it('should find product name input by its label', () => {
+            const inputElement = screen.getByLabelText('Product name');
+            expect(inputElement).not.toBeUndefined();
+        });
+
+        it('should find product count input by its label', () => {
+            const inputElement = screen.getByLabelText('Items count');
+            expect(inputElement).not.toBeUndefined();
+        });
+
+        it('should find product price input by its label', () => {
+            const inputElement = screen.getByLabelText('Price');
+            expect(inputElement).not.toBeUndefined();
+        });
+
+        it('should find product promotionalPrice input by its label', () => {
+            const inputElement = screen.getByLabelText('Promotional Price');
+            expect(inputElement).not.toBeUndefined();
+        });
     });
 });
